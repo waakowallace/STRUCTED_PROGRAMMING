90698214fae089085ced8f1990c39e460660b34e
WAAKO WALLACE WILLIAM <waakowallace19@icloud.com>
2024-11-14 15:51:30 +0300

    Add files via upload

---
+++ Data type.py
@@ -0,0 +1,17 @@
+#variable 
+pie = 3.14
+
+My_name = "katia "  
+m=
+age = 92 
+n="you are to old for this university "
+#display name from variable my_name 
+print(My_name)
+#craeting a var forage 
+print(age)
+# craeting a valaible for 2qd2zas 
+print(pie)
+print(n)
+print(m)
+
+ 
