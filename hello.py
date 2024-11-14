9797b0e24ac10bc1c7f553388b259537b53bf52b
WAAKO WALLACE WILLIAM <waakowallace19@icloud.com>
2024-11-14 15:50:24 +0300

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,18 @@
+#This bis my first oaython program
+
+
+"""
+The funcation print helps to display words
+dr"""
+
+#print("Hello structured programming class")
+
+#print   a wholr number (interger) 
+print(34)
+
+#print  a decimal number(float) 
+print(34.8)
+
+#display my name
+print("my name is WAAKO WALLACE WILLIAM")
+print("my program is BSIT")
