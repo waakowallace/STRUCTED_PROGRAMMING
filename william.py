f169e3dadbaa8f238d5871fbf41f3715ee597d70
WAAKO WALLACE WILLIAM <waakowallace19@icloud.com>
2024-11-14 15:47:42 +0300

    Add files via upload

---
+++ william.py
@@ -0,0 +1,18 @@
+name ="wallace"
+age = 22
+hight =234
+age_cpmplex= 12 + 3j
+age_in_binary=0b010101
+age_in_hex = 0x12
+age_in_octal=0o234
+sum = 2 + 1.1
+print(name, 'is of type', type(name))
+print(age, 'is of type', type(age))
+print(hight,'is of type ',type(hight))
+print(age_in_binary, 'is of type',type(age_in_binary))
+print(age_cpmplex,'is of type',type(age_cpmplex))
+print(age_in_hex),'is ot type',type(age_in_hex)
+print(age_in_octal,'is of type',type(age_in_octal))
+print(sum,'is of type',type (sum))
+wal = int (40.4)
+print(wal,'is of type',type(wal))
