b6a73a4614b10c4726f4fb03b01c7f23337ee4a6
WAAKO WALLACE WILLIAM <waakowallace19@icloud.com>
2024-11-14 15:53:48 +0300

    Add files via upload

---
+++ no6.py
@@ -0,0 +1,10 @@
+# intialize the sum
+sum=0
+while True:
+    input=input( " enter the number")
+    if input == 0:
+        #extandind the Loop
+        break
+    sum += int(int)
+    print("sum",sum)
+
