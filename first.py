76a5aef6148eb8a251500aea0b3766bcb4f0e9f5
WAAKO WALLACE WILLIAM <waakowallace19@icloud.com>
2024-11-14 15:50:39 +0300

    Add files via upload

---
+++ first.py
@@ -0,0 +1,14 @@
+#no 1
+# asking for the userers name 
+g="hallo gabriel "
+ #ask for the userers favourite colour
+favourate_colour="what is your fav colour"
+print(g)
+print(favourate_colour)
+
+#No 2
+# finding the area 
+length=int(input("entre the length od the triangle:"))
+width=int(input("entre the width of the rectangle:"))
+area=length*width
+print(f"the area of the rectangle is :{area}")
