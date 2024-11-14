789591b44dae97233ecf670ea129d126ab0e9d23
WAAKO WALLACE WILLIAM <waakowallace19@icloud.com>
2024-11-14 15:54:09 +0300

    Add files via upload

---
+++ number 2.py
@@ -0,0 +1,11 @@
+age =25
+book_price = 2500.0
+name = "WAAKO"
+libery_membership= bool('true , false')
+print(age,'is of type',type (age))
+print(book_price,' is of type',type(book_price))
+print(name,'is of type',type(name))
+print(libery_membership,'is of type',type(libery_membership))
+
+
+
