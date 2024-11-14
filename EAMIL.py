848851491182f1c8327751172983db585f737b12
WAAKO WALLACE WILLIAM <waakowallace19@icloud.com>
2024-11-14 15:51:20 +0300

    Add files via upload

---
+++ EAMIL.py
@@ -0,0 +1,13 @@
+#print your firts name
+name=input("enter your first name :")
+second_name=input("enter your second name :")
+
+# enter your email
+email=input("enter your email:")
+password=input("enter youe email password")
+
+# print for password
+if   password is ("correct password"):
+    print("welcome to your gmail")
+else:
+    print("wrong password")
