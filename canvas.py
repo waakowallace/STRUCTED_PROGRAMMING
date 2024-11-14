710e1221ba96a2ace1f63ffb5cbacada03513144
WAAKO WALLACE WILLIAM <waakowallace19@icloud.com>
2024-11-14 15:52:07 +0300

    Add files via upload

---
+++ canvas.py
@@ -0,0 +1,28 @@
+#No 2 
+#SUmming up all the numbers in a list 
+number=[8,2,3,0,7]
+result=sum(number)
+print(result)
+
+#NO16
+#EXEC a cord in a sring 
+name="WAAKO WALLACE WILLIAM"
+code_string="print('WAAKO WALLACE')"
+exec(code_string)
+#NO17
+#accessing a funcation inside another funcation
+def first_funcation():
+    def second():
+        print("waako wallace william ")
+    second()
+#NO 6
+# Check if 5 falls within the range of 1 to 10
+result = range(5, 1, 10)
+print(result)  # Output: True
+
+# Check if 15 falls within the range of 1 to 10
+result = range(15, 1, 10)
+print(result) 
+
+
+ 
