9d3669b21ba18751afd8eacbfeb0633bdadd3ae2
WAAKO WALLACE WILLIAM <waakowallace19@icloud.com>
2024-11-14 15:49:26 +0300

    Add files via upload

---
+++ number 3.py
@@ -0,0 +1,9 @@
+age = 25+5
+book_price = 2500.0*3
+name_first= "waako"
+second_nam= "smith"
+dh="waako"++"smith"
+
+print(age,'is of type',type (age))
+print(book_price,' is of type',type(book_price))
+print(dh,'is of type',type(dh))
